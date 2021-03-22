78c1f88ac3cda7552316f42515e2be6691389268
Marcos André <=>
2021-03-22 14:51:49 -0300

    Finalizado

---
+++ Atividade/Atividade/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using Atividade.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Atividade.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public AccountController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Manage() 
+        {
+            return View();
+        }
+
+        public IActionResult ForgotPassword() 
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
